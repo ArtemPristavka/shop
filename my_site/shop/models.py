5bb250f975f5ab217d6746a60069b88f8cd277c6
artiom_pristavka <artem_pristavka@mail.ru>
2024-10-06 18:30:54 +0300

    created models Category and Product

--- my_site/shop/models.py
+++ my_site/shop/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    "Model Category"
+    
+    name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateField(auto_now_add=True)
+
+
+class Product(models.Model):
+    "Model Product"
+    
+    name = models.CharField(max_length=80)
+    description = models.TextField(max_length=600)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    created_at = models.DateField(auto_now_add=True)
+    archived = models.BooleanField(default=True)
