758c952ab791b0fde5364df62d61559315daec8f
artiom_pristavka <artem_pristavka@mail.ru>
2024-10-07 00:35:46 +0300

    add template for detail-product and css

--- my_site/shop/urls.py
+++ my_site/shop/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from .views import ProductDetailView
+
 
 app_name = "shop"
 
 urlpatterns = [
-    
+    path("product/<int:pk>/", ProductDetailView.as_view(), name="product-detail"),
 ]
--- my_site/shop/views.py
+++ my_site/shop/views.py
@@ -1,3 +1,18 @@
+from typing import Any
+from django.db.models.base import Model as Model
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from .models import Product
+
+
+
+class ProductDetailView(DetailView):
+    "View about detail model Product"
+    
+    model = Product
+    context_object_name = "product"
+    template_name = "shop/detail-product.html"
+    
+    
