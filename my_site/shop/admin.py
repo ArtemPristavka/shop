fdeef60ca5c594cf278a94b60724a6152c2ec285
artiom_pristavka <artem_pristavka@mail.ru>
2024-10-08 17:10:34 +0300

    add news action in Admin Products archiving/unarchiving and global disable action 'delete'

--- my_site/shop/admin.py
+++ my_site/shop/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from django.http import HttpRequest
+from django.db.models import QuerySet
 
 from .models import Category, Product, ProductImage
+
+
+
+admin.site.disable_action("delete_selected")
 
 
 @admin.register(Category)
@@ -24,7 +30,7 @@ class ProductAdmin(admin.ModelAdmin):
     "Model Product for Admin site"
     
     fields = ["id", "category", "name", "description", "price", "created_at", "archived"]
-    list_display = ["id", "name", "price"]
+    list_display = ["id", "name", "price", "archived"]
     list_display_links = ["id", "name"]
     readonly_fields = ["id", "created_at"]
     search_fields = ["name__icontains", "description__icontains"]
@@ -33,4 +39,27 @@ class ProductAdmin(admin.ModelAdmin):
     inlines = [
         ProductImageInline
     ]
+    
+    actions = ["make_archived_products", "make_unarchiving_products"]
+    
+    @admin.action(description="Archiving products")
+    def make_archived_products(self, request: HttpRequest, queryset: QuerySet) -> None:
+        "Archiving products"
+        
+        updated = queryset.update(archived=True)
+        self.message_user(
+            request=request,
+            message=f"Successfull maked archived products in cout: {updated}"
+        )
+        
+    @admin.action(description="Unarchinig products")
+    def make_unarchiving_products(self, request: HttpRequest, queryset: QuerySet) -> None:
+        "Unarchiving products"
+        
+        updated = queryset.update(archived=False)
+        self.message_user(
+            request=request,
+            message=f"Successfull maked unarchived products in cout: {updated}"
+        )
+            
     
