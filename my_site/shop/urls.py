48c281f1aec1143ae04611c2d279b5449f710191
artiom_pristavka <artem_pristavka@mail.ru>
2024-10-08 15:51:50 +0300

    create new path by category products and add new template

--- my_site/shop/urls.py
+++ my_site/shop/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from .views import ProductDetailView
+from .views import ProductDetailView, ProductListView, ProductListByCategoryView
 
 
 app_name = "shop"
 
 urlpatterns = [
     path("product/<int:pk>/", ProductDetailView.as_view(), name="product-detail"),
+    path("products/", ProductListView.as_view(), name="products-list"),
+    path(
+        "products/<int:category>/", 
+        ProductListByCategoryView.as_view(), 
+        name="products-list-by-category"
+    )
 ]
--- my_site/shop/views.py
+++ my_site/shop/views.py
@@ -2,9 +2,11 @@ from typing import Any
 from django.db.models.base import Model as Model
 # from django.db.models.query import QuerySet
 # from django.shortcuts import render
+from django.db.models.query import QuerySet
+from django.http import HttpRequest, HttpResponse
 from django.views.generic import DetailView, ListView
 
-from .models import Product
+from .models import Product, Category
 
 
 
@@ -16,8 +18,40 @@ class ProductDetailView(DetailView):
     template_name = "shop/detail-product.html"
     
 
-# class ProductListView(DetailView):
-#     "View list producs"
+class ProductListView(ListView):
+    "View list products"
     
-#     def get
+    queryset = Category.objects.all()
+    template_name = "shop/list-products.html"
     
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        "Add products-list"
+        
+        all_products = Product.objects.filter(archived=False).all()
+        kwargs["products_list"] = all_products
+        kwargs["show_return"] = False # Show url for return on shop:products-list
+        
+        return super().get_context_data(**kwargs)
+    
+
+class ProductListByCategoryView(ListView):
+    "View list products by category"
+    
+    queryset = Category.objects.all().only("id", "name")
+    template_name = "shop/list-products-by-category.html"
+    
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        "Add products-list and name page"
+        
+        search_category = self.kwargs["category"] # Take parametr `category` from url path
+        all_products = Product.objects.filter(
+            category=search_category,
+            archived=False
+        ).all() \
+            .only("id", "name", "description", "price", "image")
+        
+        kwargs["products_list"] = all_products
+        kwargs["category_title"] = Category.objects.get(pk=search_category)
+        kwargs["show_return"] = True # Show url for return on shop:products-list
+        
+        return super().get_context_data(**kwargs)
